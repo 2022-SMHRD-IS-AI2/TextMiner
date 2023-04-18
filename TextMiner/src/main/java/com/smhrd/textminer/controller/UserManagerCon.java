84c15b7247bce25cf991308718a1ab5efa7e4d34
SMHRD <SMHRD@121.147.52.4>
2023-04-18 12:36:13 +0900

    230418 김민현 회원정보수정삭제완료

--- TextMiner/src/main/java/com/smhrd/textminer/controller/UserManagerCon.java
+++ TextMiner/src/main/java/com/smhrd/textminer/controller/UserManagerCon.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.smhrd.textminer.dto.JoinDTO;
 import com.smhrd.textminer.dto.LoginDTO;
-import com.smhrd.textminer.dto.UserDTO;
+import com.smhrd.textminer.mapper.LoginMapper;
 import com.smhrd.textminer.mapper.UserMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -32,6 +32,9 @@ public class UserManagerCon {
 	@Autowired
 	private HttpServletRequest request;
 	
+	@Autowired
+	LoginMapper loginmapper;
+	
 	
 	@RequestMapping("/userCon")
 	public ModelAndView servletRequest(HttpServletRequest request) throws IOException {
@@ -45,27 +48,42 @@ public class UserManagerCon {
 	    
 	    // 정보수정기능
 	    if(submitType.equals("정보수정")) {
+	    	
 	        HttpSession session = request.getSession();
-	        JoinDTO loginUser = (JoinDTO)session.getAttribute("loginUser");
+	        JoinDTO loginUser = (JoinDTO)session.getAttribute("dto");
 	        System.out.println("수행할 코드 입력");
 	        String mb_id = loginUser.getMb_id();
-	        String mb_pw = request.getParameter("mb_pw").toString();
-	        String mb_name = request.getParameter("mb_name").toString();
-	        String mb_email = request.getParameter("mb_email").toString();
-	        String mb_firm = request.getParameter("mb_firm").toString();
-	        String mb_call = request.getParameter("mb_call").toString();
+	        String mb_pw = request.getParameter("pwNb").toString();
+	        String mb_name = loginUser.getMb_name();
+	        String mb_email = request.getParameter("emailNb").toString();
+	        String mb_firm = request.getParameter("FirmNb").toString();
+	        String mb_call = request.getParameter("phoneNb").toString();
+	        String mb_key1 = loginUser.getMb_key1();
+	        String mb_key2 = loginUser.getMb_key2();
+	        String mb_key3 = loginUser.getMb_key3(); 
+	        String mb_region = loginUser.getMb_region();
 
-	        UserDTO dto = new UserDTO(mb_id, mb_pw, mb_name, mb_email, mb_firm, mb_call);
+	        JoinDTO dto = new JoinDTO(mb_id, mb_pw, mb_name, mb_email, mb_firm, mb_call, mb_key1, mb_key2, mb_key3, mb_region);
 
 	        int cnt = userMapper.updateUserData(dto);
 
 	        if(cnt > 0) {
 	            System.out.println("회원정보수정 성공");
-	            mav.addObject("loginUser", dto);
-	            mav.setViewName("mypage_member");
+	            
+	            //세션 다시불러오기
+	            session = request.getSession();
+	            
+	            LoginDTO loginDTO = new LoginDTO(mb_id, mb_pw);
+	            
+	            
+		        loginUser = loginmapper.selectMember(loginDTO);
+		        session.setAttribute("dto", loginUser);
+		        mav.setViewName("memberChange");
+	            return mav;
 	        } else {
 	            System.out.println("회원정보수정 실패");
-	            mav.setViewName("mypage_member");
+	            mav.setViewName("mypage");
+	            return mav;
 	        }
 	        
 	        
@@ -83,7 +101,7 @@ public class UserManagerCon {
 	        if(cnt > 0) {
 	            System.out.println("삭제 성공");
 	            session.removeAttribute("loginUser");
-	            mav.setViewName("deleteUser");
+	            mav.setViewName("memberDelete");
 	            return mav;
 	        } else {
 	            System.out.println("삭제 실패");
